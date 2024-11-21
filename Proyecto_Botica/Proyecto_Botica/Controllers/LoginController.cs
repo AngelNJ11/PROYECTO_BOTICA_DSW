e8219e87415ac33b0fa1424662e3a7d3edd965b5
Angel NJ <angelnj733@gmail.com>
2024-11-20 21:34:51 -0500

    creacion de la vista Login

---
+++ Proyecto_Botica/Proyecto_Botica/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Proyecto_Botica.Controllers
+{
+    public class Login : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
