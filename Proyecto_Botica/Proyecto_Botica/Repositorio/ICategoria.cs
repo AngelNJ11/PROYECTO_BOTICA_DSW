9a64f09bfd1ac11a55e8b852aa272bbdb41c3f8b
lenninvargas <lennin2666@gmail.com>
2024-12-06 01:40:30 -0500

    Estoy sumando la venta de productos al proyecto

---
+++ Proyecto_Botica/Proyecto_Botica/Repositorio/ICategoria.cs
@@ -0,0 +1,9 @@
+﻿using Proyecto_Botica.Models;
+
+namespace Proyecto_Botica.Repositorio
+{
+    public interface ICategoria
+    {
+        IEnumerable<Categoria> obtenerCategorias();
+    }
+}
--- Proyecto_Botica/Proyecto_Botica/obj/Debug/net8.0/Proyecto_Botica.AssemblyInfo.cs
+++ Proyecto_Botica/Proyecto_Botica/obj/Debug/net8.0/Proyecto_Botica.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto_Botica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0827a0f9a84c833947fcc786823d51e6e20cdc6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47e3a19b8e2e12ac25ed200b5f261d58d061a482")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto_Botica")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto_Botica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
